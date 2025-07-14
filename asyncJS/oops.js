b95233222e394417c66ca14a0918b7fe600dc63b
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-14 14:21:41 +0530

    oopsDone.js

--- asyncJS/oops.js
+++ asyncJS/oops.js
@@ -3,8 +3,6 @@
 // Objects contain diff attributes
 // Classes are a blueprint of an object.
 
-const { log } = require("console");
-const { workerData } = require("worker_threads");
 
 // This covers classes, objects (instance of a class) and inheritance
 
@@ -28,3 +26,46 @@ const area=rect.area();
 console.log(area);
 const peri=rect.perimeter();
 console.log(peri);
+
+
+// Inheritance in OOPs
+
+class Shape{
+    constructor(color, shape){
+        this.color=color;
+        this.shape=shape;
+    }
+    paintShape(){
+        console.log(`Painting ${this.shape} with ${this.color}`);
+    }
+    getDetails(){
+        console.log(`Your shape is ${this.shape}, painted with color ${this.color}`);
+    }
+}
+
+//taken any shape as new class that extends Shape
+// within the new constructor of the parent class we will call super(variable, "2nd parameter")
+class Rectangle extends Shape{
+    constructor(color,width, length){
+        super(color, "Rectangle");
+        this.width=width;
+        this.length=length;
+    }
+    getArea(){
+        return this.width * this.length;
+    }
+    getPerimeter(){
+        return 2*(this.width + this.length);
+    }
+    getDetails(){
+        super.getDetails();
+        console.log(`Width is ${this.width}, Length is ${this.length}`);
+    }
+}
+
+const r = new Rectangle("blue",10,20);
+const a= r.getArea();
+console.log(a);
+const p=r.getPerimeter();
+console.log(p);
+const g=r.getDetails();
