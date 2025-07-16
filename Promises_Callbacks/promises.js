8ba85c4540ebb1df91eafa7bafbc0b5c2e68d686
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-16 18:26:58 +0530

    Promises

--- Promises_Callbacks/promises.js
+++ Promises_Callbacks/promises.js
@@ -1,32 +1,31 @@
 // Promisified version of setTimeout
 
-// const setTimeoutPromisified=(ms)=>{
-//     return new Promise((resolve)=>{
-//         setTimeout(resolve,ms)
-//     });
-// }
-// const callMain=()=>{
-//     console.log("main is called");
-// }
+const setTimeoutPromisified=(ms)=>{
+    return new Promise((resolve)=>{
+        setTimeout(resolve,ms)
+    });
+}
+const callMain=()=>{
+    console.log("main is called");
+}
 
-// setTimeoutPromisified(3000).then(callMain);
+setTimeoutPromisified(3000).then(callMain);
 
 
 
 // Promisified version to read a file
-// const fs= require('fs');
+const fs= require('fs');
 
-// const readFilePromise=(filePath)=>{
-//     return new Promise((resolve, reject)=>{
-//         fs.readFile(filePath,'utf-8',(err,data)=>{
-//             console.log(data);
-//         });
-//     });
-// }
-
-// readFilePromise('./some.txt').then(message=>{
-//     console.log(message);
-// });
+const readFilePromise=(filePath)=>{
+    return new Promise((resolve, reject)=>{
+        fs.readFile(filePath,'utf-8',(err,data)=>{
+            console.log(data);
+        });
+    });
+}
+readFilePromise('./some.txt').then(message=>{
+    console.log(message);
+});
 
 
 // Promisified to greet
