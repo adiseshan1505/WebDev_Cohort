31d073bdf735799c89d38e79c65b53eb3ecb3b17
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-16 09:58:36 +0530

    setTimeout_clarified

--- Promises_Callbacks/set_timeout.js
+++ Promises_Callbacks/set_timeout.js
@@ -13,3 +13,7 @@ setTimeout(greet,3000,n);
 
 // So this code will call function greet when we call setTimeout function.
 // What it does is it will print Hello, Adiseshan after 3000ms which is 3secs. 
+// If i returned Hello, ${name} then no output will come.
+// Reason for this is that setTimeout is asynchronous and if we return something setTimeout as per the delay value
+// will immediately move to next line(s) of code.
+// Hence, only when we log the output is returned
