a7c237bd4ab961da86be4a4bb5f68821524fa0b3
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-19 10:33:36 +0530

    basic express codes

--- ExpressJS/basicExpress/basicExpress_code.js
+++ ExpressJS/basicExpress/basicExpress_code.js
@@ -1,17 +1,23 @@
 //call the express library
-const express= require('express');
-//create an instance of it
-const app= express();
+ const express= require('express');
+ const app= express();
 
-//now all api endpoints with callbacks are done here (callback is done when routes are matched)
-//these are all your route handlers.
-app.get('/', function(req, res){
-    //message
+ //get endpoint
+ app.get('/', function(req, res){
     res.send("Hello, world");
-});
-//listens on port 3000
-app.listen(3000);
+ });
 
-//run using node basicExpress_code.js
-//and test the link on Postman first:- http://localhost:3000
-//then run same link in your browser as well
+//some post endpoint
+app.post('/', function(req, res){
+    res.send("Hello from post endpoint");
+});
+
+ //asd endpoint
+ // 
+ app.get('/asd', function(req, res){
+    res.send("Hello, from asd endpoint");
+ });
+
+ app.listen(3000, ()=>{
+    console.log("Server is running on port 3000.....");
+ });
