98c6abc77e155e0810b04ce114ac10c750e6922d
Adiseshan Ramanan <adiseshanramanan@gmail.com>
2025-07-24 22:04:39 +0530

    middlewares_basics

--- ExpressJS/basicExpress/middlewares.js
+++ ExpressJS/basicExpress/middlewares.js
@@ -1,49 +1,50 @@
 //transferring request to the route handler or the api endpoint all done by next();
 
-// const express=require('express');
-// const app=express();
+const express=require('express');
+const app=express();
 
-// app.use(function(req,res, next){
-//     console.log("request has been recieved");
-//     next();
-// })
+app.use(function(req,res, next){
+    console.log("request has been recieved");
+    next();
+})
 
-// app.get('/sum/:firstArg/:secondArg', function(req,res){
-//     const a= parseInt(req.params.firstArg);
-//     const b=parseInt(req.params.secondArg);
+app.get('/sum/:firstArg/:secondArg', function(req,res){
+    const a= parseInt(req.params.firstArg);
+    const b=parseInt(req.params.secondArg);
 
-//     res.json({
-//         answer: a+b
-//     });
-// });
+    res.json({
+        answer: a+b
+    });
+});
 
-// app.listen(3000,()=>{
-//     console.log("on 3000....");
-// });
+app.listen(3000,()=>{
+    console.log("on 3000....");
+});
 
 
 // modifying and transfering the request
-// const express=require('express');
-// const app=express();
 
-// app.use(function(req,res,next){
-//     req.name="cohort";
-//     next();
-// });
+const express=require('express');
+const app=express();
 
-// app.get('/sum/:firstArg/:secondArg', function(req,res){
-//     console.log(req.name);
-//     const a= parseInt(req.params.firstArg);
-//     const b=parseInt(req.params.secondArg);
+app.use(function(req,res,next){
+    req.name="cohort";
+    next();
+});
 
-//     res.json({
-//         answer: a+b
-//     });
-// });
+app.get('/sum/:firstArg/:secondArg', function(req,res){
+    console.log(req.name);
+    const a= parseInt(req.params.firstArg);
+    const b=parseInt(req.params.secondArg);
 
-// app.listen(3000,()=>{
-//     console.log("on 3000....");
-// });
+    res.json({
+        answer: a+b
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("on 3000....");
+});
 
 
 // ending the request and response cycle just an example this is without the authentication logic that is coded in
